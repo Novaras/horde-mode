72aca1988da6e8bc6239aa48071374e96461fab9
Novaras <tfear091@gmail.com>
2022-01-26 21:27:23 +0000

    init; lvl & driver code all done + cmd station hooks

--- leveldata/campaign/say_wha/test_mission/test_mission.lua
+++ leveldata/campaign/say_wha/test_mission/test_mission.lua
@@ -1,54 +1,140 @@
 if (modkit == nil) then dofilepath("data:scripts/modkit.lua"); end
 
 rules = modkit.campaign.rules;
+map = modkit.campaign.map;
 
-local rule_a = rules:make(
-	"A",
-	function (self)
-		local gametime = Universe_GameTime();
-		print("hello from rule fn A! (" .. gametime .. ")");
+rules:init("data:leveldata/campaign/say_wha/test_mission/test_mission.level");
 
-		if (gametime > 10) then
-			return "done bruh";
+dofilepath("data:scripts/custom_code/horde/phase_rewards.lua");
+
+---@class Wave
+---@field value integer
+---@field enemy_types string[]
+---@field rewards WaveReward
+
+---@class Phase
+---@field waves Wave[]
+
+---@type table<integer, Phase>
+local PHASE_CONFIGS = {
+	[1] = {
+		waves = {
+			[1] = {
+				value = 1000,
+				enemy_types = {
+					"vgr_interceptor",
+					"tai_multiguncorvette"
+				},
+				rewards = WAVE_REWARDS[0]
+			},
+			[2] = {
+				value = 1500,
+				enemy_types = {
+					"hgn_assaultfrigate",
+					"kus_attackbomber",
+					"kus_lightcorvette"
+				},
+				rewards = WAVE_REWARDS[1]
+			},
+		},
+	},
+	[2] = {
+		waves = {
+			[1] = {
+				value = 4000,
+				enemy_types = {
+					"tai_destroyer"
+				},
+				rewards = WAVE_REWARDS[2]
+			}
+		},
+	}
+};
+
+local phase_rules = {};
+-- here we set up the phase rules
+for index, phase in PHASE_CONFIGS do
+	phase_rules[index] = rules:make(
+		"phase_" .. index,
+		function (state)
+			if (state._tick == 1) then
+				Subtitle_Message("hello from phase " .. %index, 3);
+				modkit.table.printTbl(state, "rule state");
+			end
+			if (Universe_GameTime() > state._started_gametime + 10) then
+				print("gametime " .. Universe_GameTime() .. " is +10 on our start time: " .. state._started_gametime);
+				return %index;
+			end
+		end,
+		1
+	);
+end
+
+local phase_manager_rule = rules:make(
+	"phase_manager",
+	function (state, rules)
+		print("hello from phase manager!");
+
+		if (state.phase_rules == nil) then
+			print("manager setting up...");
+			modkit.table.printTbl(
+				modkit.table.keys(%phase_rules),
+				"phase rules"
+			);
+
+			state.phase_rules = %phase_rules;
+
+			function state:allFinished()
+				return modkit.table.all(
+					self.phase_rules,
+					---@param rule Rule
+					function (rule)
+						return rule.status == "returned";
+					end
+				);
+			end
+
+			function state:phaseBegin(rules, index)
+				print("beginning phase " .. index);
+				local phase_rule = self.phase_rules[index];
+				if (phase_rule ~= nil) then
+					self.running = "phase_" .. index;
+					rules:begin(phase_rule);
+					rules:on(
+						self.running,
+						function ()
+							%self:phaseEnded(%rules, %index);
+						end
+					);
+					print("\tpattern: '" .. self.running .. "'");
+				else
+					print("no such rule: phase_" .. index .. ", do nothing...");
+				end
+			end
+
+			---@param rules Rules
+			function state:phaseEnded(rules, index)
+				print("phase " .. index .. " ended, callback triggered! (pattern: '" .. self.running .. "')");
+				self:phaseBegin(rules, index + 1);
+			end
+		end
+
+		if (state:allFinished()) then
+			return "yata";
+		end
+
+		if (state.running == nil) then
+			print("no phase running!");
+			state:phaseBegin(rules, 1);
 		end
 	end,
 	1
 );
 
-local rule_b = rules:make(
-	"my_rule",
-	function (self)
-		local gametime = Universe_GameTime();
-		print("hello from rule fn my_rule! (" .. gametime .. ")");
-
-		if (random() < 0.5) then
-			return "my_rule fin";
-		end
-	end,
-	1
+rules:begin(phase_manager_rule);
+rules:on(
+	phase_manager_rule.id,
+	function ()
+		Subtitle_Message("all done!", 5);
+	end
 );
-
-local rule_c = rules:make(
-	"C2",
-	function (self)
-		local gametime = Universe_GameTime();
-		print("hello from rule fn C2! (" .. gametime .. ")");
-
-		if (random() < 0.1) then
-			return "C2 finish";
-		end
-	end,
-	1
-);
-
-rules:begin(rule_a);
-rules:begin(rule_b);
-rules:begin(rule_c);
-
-rules:on("A and (my_rule or C2)", function (rules)
-	print("first listener!");
-end);
-
-rules:on("(A and my_rule) or C2", function (rules)
-	print("second listener!");
-end);
