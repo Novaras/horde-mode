7f9af369025c2e267c9d0bd63771fd3fe37ac913
Novaras <tfear091@gmail.com>
2022-02-15 15:24:04 +0000

    reorganise ship scripts a little

---
+++ scripts/custom_code/horde/ship_scripts/bomber_cloaks.lua
@@ -0,0 +1,18 @@
+---@class AttackBomberProto : Ship
+atk_bomber_proto = {
+	previous_hp = 1
+};
+
+function atk_bomber_proto:update()
+	if (self.player:hasResearch("Bomber_Cloaking")) then
+		if (self:canDoAbility(AB_Cloak) == 0) then
+			self:canDoAbility(AB_Cloak, 1);
+		end
+
+		if (self:isCloaked() == 0) then
+			self:HP(self:HP() + 0.02);
+		end
+	end
+end
+
+modkit.compose:addShipProto("hgn_attackbomber", atk_bomber_proto);
--- scripts/custom_code/horde/ship_scripts/commstation.lua
+++
@@ -1,6 +0,0 @@
----@class CommStationProto : Ship
-commstation_proto = {};
-
-modkit.compose:addShipProto("horde_commstation", commstation_proto);
-
-print("commstation should be loaded");
